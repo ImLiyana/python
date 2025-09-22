6a7eac846b4895ffa5f14df6135f9731296d1f03
ImLiyana <160301024+ImLiyana@users.noreply.github.com>
2025-09-22 14:52:42 +0530

    Update samelen.py

--- samelen.py
+++ samelen.py
@@ -1,5 +1,6 @@
 a=[1,2,3,4]
-b=[5,6,7,4]
+b=[5,8,3,4]
+
 if(len(a)==len(b)):
    print("same length",len(a),"and ",len(b))
 else:
@@ -9,6 +10,9 @@ if(sum(a)==sum(b)):
     print("same sum, sum= ",sum(a))
 else:
     print("sum is not same sum of a = ",sum(a),"sum of b = ",sum(b))
-s=a,b
-for i in s:
-    print(i,s.count(i))
+
+for i in a:
+   for j in b:
+      if i==j:
+         print("repeated ",i,j)
+                                    
