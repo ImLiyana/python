54e15cc3cb3e34a90d6c762f83c473513e42e6a8
ImLiyana <160301024+ImLiyana@users.noreply.github.com>
2025-09-22 14:55:23 +0530

    Add files via upload

---
+++ dictmult.py
@@ -0,0 +1,6 @@
+d={1:2,2:3,3:4,4:5}
+s=1
+a=d.values()
+for i in a:
+    s=s*i
+print(s)
---
+++ mergedict.py
@@ -0,0 +1,6 @@
+a={1:"apple",2:"orange",3:"cherry"}
+b={4:"red",5:"white",6:"pink"}
+c={}
+c=a.copy()
+c.update(b)
+print(c)
